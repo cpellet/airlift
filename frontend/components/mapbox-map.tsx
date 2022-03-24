5dd0f9b8bdd52a117fa997401989415bfa0579de
Cyrus Pellet <cyrus135@hotmail.fr>
2022-03-24 20:29:10 +0100

    Overlay action menu

--- frontend/components/mapbox-map.tsx
+++ frontend/components/mapbox-map.tsx
@@ -1,12 +1,13 @@
 import { Component } from 'react';
-import Map from 'react-map-gl';
+import Map, { Source, Layer } from 'react-map-gl';
+import type { SkyLayer } from 'react-map-gl';
 
 class MapboxMap extends Component {
 
     render() {
         return (
+            <>
             <Map
-                reuseMaps
                 mapStyle="mapbox://styles/mapbox/streets-v9"
                 initialViewState={{
                     latitude: 48.711560,
@@ -14,7 +15,9 @@ class MapboxMap extends Component {
                     zoom: 14}}
                 style={{ width: "fit", height: "94vh" }}
                 mapboxAccessToken="pk.eyJ1IjoiY3BlbGxldCIsImEiOiJjaXAyNjRiOG4wMDA2dnpseXEzZWwxcGpkIn0.Mx1G74kM528rmSrUP4XCVQ"
-            />
+                />
+               
+            </>
         );
     }
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from 'next'
-import { AppShell, Header, Text, Navbar, Group, AspectRatio } from '@mantine/core';
+import { AppShell, Header, Text, Navbar, Button, Group, AspectRatio } from '@mantine/core';
+import { Home, Dashboard, FileText, Search, Route } from 'tabler-icons-react';
+import type { SpotlightAction } from '@mantine/spotlight';
+import { SpotlightProvider, useSpotlight } from '@mantine/spotlight';
 import MapboxMap from "../components/mapbox-map";
 import { useState } from 'react';
 import Head from 'next/head'
@@ -7,25 +10,51 @@ import Image from 'next/image'
 import dynamic from 'next/dynamic';
 import styles from '../styles/Home.module.css'
 
-const DynamicComponentWithNoSSR = dynamic(() => import('../components/mapbox-map'), {
+const DynamicMapNoSSR = dynamic(() => import('../components/mapbox-map'), {
   ssr: false
 });
 
-const Home: NextPage = () => {
+function SpotlightControl() {
+  const spotlight = useSpotlight();
   return (
-    <AppShell
-    padding={0}
-      header={<Header height={"6vh"} p="xs"><Group><Image src="/../public/airlift.png" alt="Airlift logo" width='40px' height='40px' layout="fixed"></Image><Text variant="gradient"
-        gradient={{ from: 'cyan', to: 'pink', deg: 45 }}
-        size="xl"
-        weight={400}>AIRLIFT</Text></Group></Header>}
-      styles={(theme) => ({
-        main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
-      })}
+    <Group position="center">
+      <Button onClick={spotlight.openSpotlight}>Open spotlight</Button>
+    </Group>
+  );
+}
+
+const actions: SpotlightAction[] = [
+  {
+    title: 'Auto Bus',
+    description: 'Generate bus routes for this area',
+    onTrigger: () => console.log('Home'),
+    icon: <Route size={18} />,
+  }
+];
+
+const HomePage: NextPage = () => {
+  return (
+     <SpotlightProvider
+      actions={actions}
+      searchIcon={<Search size={18} />}
+      searchPlaceholder="Search..."
+      shortcut="shift + space"
+      nothingFoundMessage="Nothing found..."
     >
-      {<DynamicComponentWithNoSSR></DynamicComponentWithNoSSR>}
-    </AppShell>
+      <AppShell
+        padding={0}
+        header={<Header height={"6vh"} p="xs"><Group><Image src="/../public/airlift.png" alt="Airlift logo" width='40px' height='40px' layout="fixed"></Image><Text variant="gradient"
+          gradient={{ from: 'cyan', to: 'pink', deg: 45 }}
+          size="xl"
+          weight={400}>AIRLIFT</Text></Group></Header>}
+        styles={(theme) => ({
+          main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+        })}
+      >
+        {<DynamicMapNoSSR />}
+      </AppShell>
+    </SpotlightProvider>
   )
 }
 
-export default Home
+export default HomePage
