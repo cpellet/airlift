2a4552e3b9490fdf08ca7374af0d69f5d8d42784
Cyrus Pellet <cyrus135@hotmail.fr>
2022-04-15 16:28:23 +0200

    Analysis button and notifications

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { AppShell, Menu, Header, Text, Button, Group, Divider, Drawer, Image, MantineProvider, ColorSchemeProvider, ColorScheme, ActionIcon } from '@mantine/core';
+import { NotificationsProvider, showNotification, updateNotification } from '@mantine/notifications';
 import { useLocalStorage } from '@mantine/hooks';
-import { Search, Route, PlayerPlay, Sun, MoonStars, Cpu } from 'tabler-icons-react';
+import { Search, Route, PlayerPlay, Sun, MoonStars, Cpu, X } from 'tabler-icons-react';
 import { SpotlightProvider } from '@mantine/spotlight';
 import './App.css';
 import SpotlightControl from './components/spotlight-control';
@@ -8,12 +9,34 @@ import MapboxMap from './components/mapbox-map';
 import { useEffect, useState } from "react";
 import axios from 'axios';
 import StatusPanel from './components/status-panel';
+import { usePromiseTracker, trackPromise} from 'react-promise-tracker';
 
 async function fetchAirliftServerData(url: string) {
   return axios.get(url).then(function (res) { return res.data; }).catch(function (error) {
     return {};
   });
 };
+
+async function requestAnalysis(){
+  showNotification({
+    id: 'load-data',
+    loading: true,
+    title: 'Region analysis',
+    message: 'Analysis will start with the selected signals...',
+    autoClose: false,
+    radius: 'lg',
+    disallowClose: true,
+  });
+  await new Promise(resolve => setTimeout(resolve, 3000));
+  updateNotification({
+    id: 'load-data',
+    color: 'red',
+    title: 'Analysis failed',
+    message: 'Could not connect to server',
+    icon: <X />,
+    autoClose: 2000,
+  });
+}
 
 const algo_type = {name: '', descr: ''};
 
@@ -54,75 +77,83 @@ function App() {
     setServerState();
   },[])
 
+  const { promiseInProgress } = usePromiseTracker();
+
   return (
      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
       <MantineProvider theme={{ colorScheme }}>
-        <SpotlightProvider
-          actions={serverData.algos ? serverData.algos?.map((item, index) => ({
-            title: item.name,
-            description: item.descr,
-            onTrigger: () => {
-              setAlgo(item);
-              setDrawerOpened(true);
-            },
-            icon: <Route size={18} />,
-          })) : []}
-          searchIcon={<Search size={18} />}
-          searchPlaceholder="Search..."
-          shortcut="shift + space"
-          nothingFoundMessage="Nothing found..."
-        >
-          <Drawer
-            opened={drawerOpened}
-            onClose={() => setDrawerOpened(false)}
-            title={<Text size='xl' transform="uppercase">{selectedAlgo.name}</Text>}
-            padding="xl"
-            size="xl"
+        <NotificationsProvider>
+          <SpotlightProvider
+            actions={serverData.algos ? serverData.algos?.map((item, index) => ({
+              title: item.name,
+              description: item.descr,
+              onTrigger: () => {
+                setAlgo(item);
+                setDrawerOpened(true);
+              },
+              icon: <Route size={18} />,
+            })) : []}
+            searchIcon={<Search size={18} />}
+            searchPlaceholder="Search..."
+            shortcut="shift + space"
+            nothingFoundMessage="Nothing found..."
           >
-            <Text>Configuration:</Text>
-            <Button leftIcon={<PlayerPlay />} color="teal" size="lg">
-              Run
-            </Button>
-          </Drawer>
-          <AppShell
-            padding={0}
-            header={<Header height={"55px"} p="xs"><Group position="apart"><Group><Image src='/airlift.png' alt="Airlift logo" width='30px' height='30px'></Image><Text variant="gradient"
-              gradient={{ from: 'cyan', to: 'pink', deg: 160 }}
+            <Drawer
+              opened={drawerOpened}
+              onClose={() => setDrawerOpened(false)}
+              title={<Text size='xl' transform="uppercase">{selectedAlgo.name}</Text>}
+              padding="xl"
               size="xl"
-              weight={700}>AIRLIFT</Text></Group><Group><Menu control={<Button leftIcon={<PlayerPlay />} variant="subtle">
+            >
+              <Text>Configuration:</Text>
+              <Button leftIcon={<PlayerPlay />} color="teal" size="lg">
                 Run
-              </Button>}>
-                <Menu.Label>Transportation</Menu.Label>
-                {
-                  serverData.algos?.map((item, index) => (
-                    <Menu.Item icon={<Route size={14} />} onClick={() => {
-                      setAlgo(item);
-                      setDrawerOpened(true);
-                    }}>{item.name}</Menu.Item>
-                  ))
-                }
-                <Divider />
-                <SpotlightControl />
-              </Menu>
-                <Button variant="light" color="green" radius="md" leftIcon={<Cpu size={18} />}>
-                  Analyze
-                </Button>
-              <ActionIcon
-                  radius="xl"
-                  variant="hover"
-                  color={dark ? 'yellow' : 'blue'}
-                  onClick={() => toggleColorScheme()}
-                  title="Toggle color scheme"
-                >
-                  {dark ? <Sun size={20} /> : <MoonStars size={20} />}
-                </ActionIcon></Group></Group></Header>}
-            styles={(theme) => ({
-              main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
-            })}
-          >
-            <MapboxMap theme={colorScheme} /><StatusPanel status={serverData.status} host={serverData.host} version={serverData.version} retry={setServerState} theme={colorScheme}/>
-          </AppShell>
-        </SpotlightProvider>
+              </Button>
+            </Drawer>
+            <AppShell
+              padding={0}
+              header={<Header height={"55px"} p="xs"><Group position="apart"><Group><Image src='/airlift.png' alt="Airlift logo" width='30px' height='30px'></Image><Text variant="gradient"
+                gradient={{ from: 'cyan', to: 'pink', deg: 160 }}
+                size="xl"
+                weight={700}>AIRLIFT</Text></Group><Group><Menu control={<Button leftIcon={<PlayerPlay />} variant="subtle">
+                  Run
+                </Button>}>
+                  <Menu.Label>Transportation</Menu.Label>
+                  {
+                    serverData.algos?.map((item, index) => (
+                      <Menu.Item icon={<Route size={14} />} onClick={() => {
+                        setAlgo(item);
+                        setDrawerOpened(true);
+                      }}>{item.name}</Menu.Item>
+                    ))
+                  }
+                  <Divider />
+                  <SpotlightControl />
+                </Menu>
+                  <Button variant="light" color="green" radius="md" leftIcon={<Cpu size={18} />} loading={promiseInProgress} onClick={() => {
+                    trackPromise(
+                      requestAnalysis()
+                    );
+                  }}>
+                    Analyze
+                  </Button>
+                  <ActionIcon
+                    radius="xl"
+                    variant="hover"
+                    color={dark ? 'yellow' : 'blue'}
+                    onClick={() => toggleColorScheme()}
+                    title="Toggle color scheme"
+                  >
+                    {dark ? <Sun size={20} /> : <MoonStars size={20} />}
+                  </ActionIcon></Group></Group></Header>}
+              styles={(theme) => ({
+                main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+              })}
+            >
+              <MapboxMap theme={colorScheme} /><StatusPanel status={serverData.status} host={serverData.host} version={serverData.version} retry={setServerState} theme={colorScheme} />
+            </AppShell>
+          </SpotlightProvider>
+        </NotificationsProvider>
       </MantineProvider>
     </ColorSchemeProvider>
   );
